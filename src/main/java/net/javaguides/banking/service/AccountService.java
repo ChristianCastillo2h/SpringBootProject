82c5e1efb068bf3de13c11cfb0e71934cbda5565
ChristianCastillo2h <christiancastillo2h@gmail.com>
2024-07-08 22:06:55 -0600

    Carga Final

--- src/main/java/net/javaguides/banking/service/AccountService.java
+++ src/main/java/net/javaguides/banking/service/AccountService.java
@@ -1,9 +1,21 @@
 package net.javaguides.banking.service;
 
-import net.javaguides.banking.entity.Account;
+import net.javaguides.banking.dto.AccountDto;
+
+import java.util.List;
 
 public interface AccountService {
 
-    Account createAccount(Account account);
+    AccountDto createAccount(AccountDto accountDto);
+
+    AccountDto getAccountById(Long id);
+
+    AccountDto deposit(Long id,double amount);
+
+    AccountDto withdraw(Long id,double amount);
+
+    List<AccountDto> getAllAccounts();
+
+    void deleteAccount(Long id) ;
 }
 
